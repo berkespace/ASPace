6a089130c4345d4b2113738b059509cb9bf7efd1
Berke Space <stelberke@gmail.com>
2022-11-21 09:13:44 +0300

    Finished adding the sections

---
+++ ASPace/ViewComponents/Experience/ExperienceView.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPace.ViewComponents.Experience
+{
+    public class ExperienceView:ViewComponent
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+
+    }
+}
